c338075acaa352dfbcedc2eeff3627fe44f591d8
JeresB <boiselet.jeremy@gmail.com>
2017-04-07 11:08:41 +0200

    suppsection.php

--- suppsection.php
+++ suppsection.php
@@ -1,11 +1,16 @@
 <?php
+  // Inclusion de la connexion à la BDD
   include 'loginBDD.php';
 
+  // Requete de suppression de la section
   $requete = "DELETE FROM section WHERE `section` = :section";
+  // On recupère le nom de la section à supprimer
   $section = htmlspecialchars($_POST["section"]);
 
+  // Préparation et execution de la suppression
   $query = $pdo->prepare($requete);
   $query->bindParam(":section", $section, PDO::PARAM_STR, 15);
   $query->execute();
 
+  // Redirection vers la liste des sections
   header('Location: section.php');
