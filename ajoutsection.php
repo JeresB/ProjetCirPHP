42a131d2791de999e3fda351ec700dc0a73ee3c0
JeresB <boiselet.jeremy@gmail.com>
2017-04-07 11:32:01 +0200

    ajoutsection.php

--- ajoutsection.php
+++ ajoutsection.php
@@ -1,13 +1,15 @@
 <?php
-
+  // Inclusion : connexion à la BDD et header Html
   include 'loginBDD.php';
   include 'header.php';
 
+  // Titre
   echo "<div class='container'>
         <div class='page-header'>
           <h1>Ajouter une section <small>Back office</small></h1>
         </div>";
 
+  // Formulaire d'ajout d'une section
   echo '<form class="form-horizontal" method="post" action="ajoutsection.php">
           <div class="form-group">
             <label class="control-label col-sm-2" for="section">Section :</label>
@@ -22,15 +24,20 @@
           </div>
         </form>';
 
+  // On récupère de le nom de la section
   $section = htmlspecialchars($_POST["section"]);
 
+  // Si le nom de la section existe
   if (isset($_POST["section"])) {
+    // Préparation et execution de la requête
     $requete = "INSERT INTO section (section) VALUES (:section)";
     $query = $pdo->prepare($requete);
     $query->bindParam(":section", $section, PDO::PARAM_STR, 15);
     $query->execute();
 
+    // Redirection vers la liste des sections
     header('Location: section.php');
   }
 
+  // Inclusion du footer Html
   include 'footer.php';
