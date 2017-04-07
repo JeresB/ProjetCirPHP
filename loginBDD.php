63d7720f4b0d3289160eb6ece5ac30977f712193
JeresB <boiselet.jeremy@gmail.com>
2017-04-07 11:15:46 +0200

    loginBDD.php

--- loginBDD.php
+++ loginBDD.php
@@ -1,8 +1,16 @@
 <?php
+// Connexion à la base de données
+// HOST : localhost
+// PORT : 3306
+// NOM : pdo
+// CHARSET : utf8
+// LOGIN : pdo
+// MOT DE PASSE : phpmyadmin
 $myDsn = "mysql:host=localhost;port=3306;dbname=pdo;charset=utf8;";
 $myDbLogin = "pdo";
 $myDbPwd = "phpmyadmin";
 
+// Création de l'objet PDO
 try {
   $pdo = new PDO($myDsn, $myDbLogin, $myDbPwd);
 } catch (PDOException $e) {
