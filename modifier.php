fbe7d545982c38db5b72fda9ae57dc8a0fa863b0
cicotetchi <jeanfrancoistetchi@yahoo.fr>
2017-05-02 16:35:26 +0200

    Update modifier.php

--- modifier.php
+++ modifier.php
@@ -1,151 +1,101 @@
 <?php
-
   include 'loginBDD.php';
   include 'header.php';
-
-  $old_nom = htmlspecialchars($_POST["nom"]);
-  $old_prenom = htmlspecialchars($_POST["prenom"]);
-
-  $query = $pdo->prepare("SELECT * FROM etudiant WHERE nom = '".$old_nom."' AND prenom = '".$old_prenom."'");
-  $query->execute();
-  $row = $query->fetch();
-
+  $old_nom = $_POST["nom"];
+  $old_prenom = $_POST["prenom"];
+  
   echo "<div class='container'>
         <div class='page-header'>
-          <h1>Modifer l'étudiant : ".$old_nom." ".$old_prenom." <small> Back office <a href = 'etudiants.php' class='btn btn-default'>Retour</a></small></h1>
+          <h1>Modifer l'étudiant : ".$old_nom." ".$old_prenom." <small> Back office</small></h1>
         </div>";
-
+        $query = $pdo->prepare("SELECT * FROM etudiant WHERE `nom` = '".$_POST['nom']."' AND `prenom` = '".$_POST['prenom']."'");
+        $query->execute();
+        $row = $query->fetch();
+        $query_section = $pdo->prepare("SELECT * FROM section");
+        $query_section->execute();
+        
   echo '<form class="form-horizontal" method="post" action="modifier.php">
           <div class="form-group">
-            <label class="control-label col-sm-2" for="nouveau_nom">Nouveau Nom :</label>
+            <label class="control-label col-sm-2" for="nouveau_nom">Nom:</label>
               <div class="col-sm-10">
-                <input type="text" class="form-control" name = "nouveau_nom" id="nouveau_nom">
+              <input type="text" class="form-control" name = "nouveau_nom" id="nouveau_nom" value="'.$row['nom'].'" >
               </div>
           </div>
-          <input type="hidden" name="nom" value='.$row["nom"].' />
-          <input type="hidden" name="prenom" value='.$row["prenom"].' />
+          <input type="hidden" class="form-control" name = "nom" id="nouveau_nom" value="'.$row['nom'].'" >
           <div class="form-group">
-            <div class="col-sm-offset-2 col-sm-10">
-              <input type="submit" class="btn btn-default" value = "Modifier le nom" />
-            </div>
-          </div>
-        </form>
-
-        <form class="form-horizontal" method="post" action="modifier.php">
-           <div class="form-group">
-             <label class="control-label col-sm-2" for="nouveau_prenom">Nouveau Prenom :</label>
-               <div class="col-sm-10">
-                 <input type="text" class="form-control" name = "nouveau_prenom" id="nouveau_prenom">
-               </div>
-           </div>
-           <input type="hidden" name="nom" value='.$row["nom"].' />
-           <input type="hidden" name="mail" value='.$row["mail"].' />
-           <div class="form-group">
-             <div class="col-sm-offset-2 col-sm-10">
-               <input type="submit" class="btn btn-default" value = "Modifier le prenom" />
-             </div>
-           </div>
-         </form>
-
-         <form class="form-horizontal" method="post" action="modifier.php">
-            <div class="form-group">
-               <label class="control-label col-sm-2" for="mail">Nouveau Mail :</label>
-               <div class="col-sm-10">
-                  <input type="mail" class="form-control" name = "mail" id="mail">
-                </div>
-             </div>
-            <input type="hidden" name="nom" value='.$row["nom"].' />
-            <input type="hidden" name="prenom" value='.$row["prenom"].' />
-            <div class="form-group">
-              <div class="col-sm-offset-2 col-sm-10">
-                <input type="submit" class="btn btn-default" value = "Modifier l\'adresse mail" />
-              </div>
-            </div>
-          </form>
-
-          <form class="form-horizontal" method="post" action="modifier.php">
-          <div class="form-group">
-            <label class="control-label col-sm-2" for="date">Nouvelle date d\'anniversaire :</label>
+            <label class="control-label col-sm-2" for="nouveau_nom">prenom:</label>
               <div class="col-sm-10">
-                <input type="date" class="form-control" name = "date" id="date">
+              <input type="text" class="form-control" name = "nouveau_prenom" id="nouveau_nom" value="'.$row['prenom'].'" >
               </div>
           </div>
-             <input type="hidden" name="nom" value='.$row["nom"].' />
-             <input type="hidden" name="prenom" value='.$row["prenom"].' />
-             <div class="form-group">
-               <div class="col-sm-offset-2 col-sm-10">
-                 <input type="submit" class="btn btn-default" value = "Modifier la date de naissance" />
-               </div>
-             </div>
-           </form>
-
-           <form class="form-horizontal" method="post" action="modifier.php">
-           <div class="form-group">
-             <label class="control-label col-sm-2" for="section">Nouvelle section :</label>
-               <div class="col-sm-10">
-                 <input type="text" class="form-control" name = "section" id="section">
-               </div>
-           </div>
-              <input type="hidden" name="nom" value='.$row["nom"].' />
-              <input type="hidden" name="prenom" value='.$row["prenom"].' />
-              <div class="form-group">
-                <div class="col-sm-offset-2 col-sm-10">
-                  <input type="submit" class="btn btn-default" value = "Modifier la section" />
-                </div>
+          <input type="hidden" class="form-control" name = "prenom" id="nouveau_nom" value="'.$row['prenom'].'" >
+          <div class="form-group">
+            <label class="control-label col-sm-2" for="nouveau_nom">Mail:</label>
+              <div class="col-sm-10">
+              <input type="text" class="form-control" name = "nouveau_mail" id="nouveau_nom" value="'.$row['mail'].'" >
               </div>
-            </form>';
-
-
-    $nom = htmlspecialchars($_POST["nouveau_nom"]);
-    $prenom = htmlspecialchars($_POST["nouveau_prenom"]);
-    $mail = htmlspecialchars($_POST["mail"]);
-    $date = htmlspecialchars($_POST["date"]);
-    $section = htmlspecialchars($_POST["section"]);
-
-
-    if (isset($_POST["nouveau_nom"])) {
-      $query = $pdo->prepare("UPDATE  `pdo`.`etudiant` SET  `nom` =  '".$nom."' WHERE  `etudiant`.`mail` =  '".$row["mail"]."' AND `etudiant`.`prenom` = '".$row["prenom"]."'");
+          </div>
+          <input type="hidden" class="form-control" name = "mail" id="nouveau_nom" value="'.$row['mail'].'" >
+          <div class="form-group">
+            <label class="control-label col-sm-2" for="nouveau_nom">Date de naissance:</label>
+              <div class="col-sm-10">
+              <input type="text" class="form-control" name = "nouveau_date" id="nouveau_nom" value="'.$row["date_naissance"].'" >
+              </div>
+          </div>
+          <input type="hidden" class="form-control" name = "date" id="nouveau_nom" value="'.$row["date_naissance"].'" >
+          <div class="form-group">
+            <label class="control-label col-sm-2" for="section">Section :</label>
+              <div class="col-sm-10">
+                <select class="form-control selectpicker" name = "nouveau_section" id = "section" required>';
+                  for ($i=0; $row_section = $query_section->fetch() ; $i++) {
+                    echo '<option>'.$row_section["section"].'</option>';
+                  }
+                echo '</select>
+              </div>
+          </div>
+          <input type="hidden" name="section" value='.$row_section.' />
+          <input type="submit"/>
+        </form>';
+            $nom = htmlspecialchars($_POST["nouveau_nom"]);
+            $prenom = htmlspecialchars($_POST["nouveau_prenom"]);
+            $mail = htmlspecialchars($_POST["nouveau_mail"]);
+            $date = htmlspecialchars($_POST["nouveau_date"]);
+            $section = htmlspecialchars($_POST["nouveau_section"]);
+            
+    if (isset($_POST["nouveau_nom"]) AND $_POST["nouveau_nom"] != $row['nom']) {
+      $query = $pdo->prepare("UPDATE  `etudiant` SET  `nom` =  '".$nom."' WHERE  `mail` =  '".$row["mail"]."'");
       $query->execute();
-
       echo '<script type="text/javascript">
               alert ("Nom modifié !");
             </script>';
     }
-
-    if (isset($_POST["nouveau_prenom"])) {
-      $query = $pdo->prepare("UPDATE etudiant SET prenom =  '".$prenom."' WHERE nom = '".$old_nom."' AND mail =  '".$mail."'");
+    if (isset($_POST["nouveau_prenom"]) AND $_POST["nouveau_prenom"] != $row['prenom']) {
+      $query = $pdo->prepare("UPDATE etudiant SET prenom =  '".$prenom."' WHERE mail =  '".$row["mail"]."'");
       $query->execute();
-
       echo '<script type="text/javascript">
               alert ("Prenom modifié !");
             </script>';
     }
-
-    if (isset($_POST["mail"]) && !isset($_POST["nouveau_prenom"])) {
-      $query = $pdo->prepare("UPDATE etudiant SET mail =  '".$mail."' WHERE nom =  '".$old_nom."' AND prenom = '".$old_prenom."'");
+    if (isset($_POST["nouveau_mail"]) AND $_POST["nouveau_mail"] != $row['mail'] ) {
+      $query = $pdo->prepare("UPDATE etudiant SET mail =  '".$mail."' WHERE mail =  '".$row["mail"]."'");
       $query->execute();
-
       echo '<script type="text/javascript">
               alert ("Adresse mail modifiée !");
             </script>';
     }
-
-    if (isset($_POST["date"]) && !isset($_POST["nouveau_prenom"])) {
-      $query = $pdo->prepare("UPDATE etudiant SET date_naissance =  '".$date."' WHERE nom =  '".$old_nom."' AND prenom = '".$old_prenom."'");
-      $query->execute();
-
+    if (isset($_POST["nouveau_date"]) AND  $_POST["nouveau_date"] != $row["date_naissance"]) {
+      $query = $pdo->prepare("UPDATE etudiant SET date_naissance =  '".$date."' WHERE mail =  '".$row["mail"]."'");
       echo '<script type="text/javascript">
               alert ("Date de naissance modifiée !");
             </script>';
     }
-
-    if (isset($_POST["section"]) && !isset($_POST["nouveau_prenom"])) {
-      $query = $pdo->prepare("UPDATE etudiant SET section =  '".$section."' WHERE nom =  '".$old_nom."' AND prenom = '".$old_prenom."'");
+    if (isset($_POST["nouveau_section"])  AND $_POST["nouveau_section"] != $row_section["section"]) {
+      $query = $pdo->prepare("UPDATE etudiant SET section =  '".$section."' WHERE mail =  '".$row["mail"]."'");
       $query->execute();
-
       echo '<script type="text/javascript">
               alert ("Section modifiée !");
             </script>';
     }
+            include 'footer.php';
 
   include 'footer.php';
